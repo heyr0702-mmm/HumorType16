cabd706c4fe6aa52b3d05bce2bd8dfc5a8f0f180
heyr0702-mmm <heyr0702@gmail.com>
2025-11-01 03:26:35 +0900

    Add GA4 initialization in app layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,41 @@
+import Script from "next/script";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {GA_MEASUREMENT_ID && (
+        <>
+          <Script
+            id="ga4-gtag"
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          />
+          <Script id="ga4-config" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_MEASUREMENT_ID}');
+            `}
+          </Script>
+        </>
+      )}
+      {/**
+       * Optional AdSense integration placeholder:
+       *
+       * <Script
+       *   id="adsense-script"
+       *   strategy="afterInteractive"
+       *   async
+       *   src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=YOUR_ADSENSE_CLIENT_ID"
+       *   crossOrigin="anonymous"
+       * />
+       */}
+      <Component {...pageProps} />
+    </>
+  );
 }
