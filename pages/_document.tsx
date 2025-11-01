d4969d026bb4319b00babb9093d8ab052d67a983
heyr0702-mmm <heyr0702@gmail.com>
2025-11-01 16:40:28 +0900

    Remove duplicate AdSense script from document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,14 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head>
-        {/* ✅ Google AdSense */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8802783650388237"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
